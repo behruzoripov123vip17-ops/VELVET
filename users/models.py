1650a8779ec2c0f1f63a8cf629bf7c54951a2694
behruzoripov123vip17-ops <behruzoripov123vip17@gmail.com>
2026-09-24 06:03:19 -0700

    Implement Google account and reservation linking: users/models.py

--- users/models.py
+++ users/models.py
@@ -1,3 +1,14 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="profile",
+    )
+    avatar = models.ImageField(upload_to="avatars/", blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.user.get_full_name() or self.user.username} profile"
