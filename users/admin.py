827e4afbb121d91f66a85e3389743db0e0a118f0
behruzoripov123vip17-ops <behruzoripov123vip17@gmail.com>
2026-09-24 06:03:21 -0700

    Implement Google account and reservation linking: users/admin.py

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import UserProfile
 
-# Register your models here.
+
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ("user", "avatar")
+    search_fields = ("user__username", "user__email", "user__first_name", "user__last_name")
